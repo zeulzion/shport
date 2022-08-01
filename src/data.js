9e32628998cdf42057441b20f3dc0c446ba1f1e7
zeulzion <106320018+zeulzion@users.noreply.github.com>
2022-08-01 22:10:01 +0100

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+    {
+      id: "uuduiouzdh",
+      name: "Basket-Ball Spalding",
+      price: 200,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing",
+      img: "images/img-1.jpg",
+    },
+    {
+      id: "uhhyloihpo",
+      name: "Jerseys",
+      price: 150,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing",
+      img: "images/img-2.jpg",
+    },
+    {
+      id: "uiggizudyho",
+      name: "Shorts",
+      price: 75,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing",
+      img: "images/img-3.jpg",
+    },
+    {
+      id: "ygyugu",
+      name: "Shoes ",
+      price: 500,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing",
+      img: "images/img-4.jpg",
+    },
+  ];
